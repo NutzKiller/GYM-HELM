3983c0ca57f20cd8b5713c1cde702c6e3744bfcb
NutzKiller <yuvalshmuely8@gmail.com>
2025-01-28 13:42:51 +0200

    work!

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,9 +28,9 @@ terraform {
 
 # Define Google Cloud provider
 provider "google" {
-  credentials = file(secret.GCP_CREDENTIALS_FILE)
-  project     = secret.GCP_PROJECT
-  region      = secret.GCP_REGION
+  credentials = file(var.GCP_CREDENTIALS_FILE) # Uses the file created from the GitHub secret
+  project     = var.GCP_PROJECT               # Passed as a variable from the workflow
+  region      = var.GCP_REGION
 }
 
 # Generate a random ID to make key names unique
