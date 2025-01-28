52d32a4d8b92cf3112b1e2ea2446ef08484d21e6
NutzKiller <yuvalshmuely8@gmail.com>
2025-01-28 12:01:51 +0200

    fix ?

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,38 @@
 # terraform/variables.tf
 
+# GCP Service Account Credentials JSON File
 variable "GCP_CREDENTIALS_FILE" {
   description = "Path to the GCP service account credentials JSON file"
   type        = string
 }
 
+# GCP Project ID
 variable "GCP_PROJECT" {
   description = "GCP Project ID"
   type        = string
 }
 
+# GCP Region
 variable "GCP_REGION" {
   description = "GCP Region"
   type        = string
 }
 
+# Database Connection String
 variable "DATABASE_URL" {
   description = "Database connection string"
   type        = string
 }
 
+# Flask Application Secret Key
 variable "SECRET_KEY" {
   description = "Flask application secret key"
   type        = string
 }
 
+# GitHub Token
 variable "MY_GITHUB_TOKEN" {
   description = "GitHub token with permissions to push to the TF repository"
   type        = string
 }
+
