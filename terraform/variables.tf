fdd1f44d29fbaaae6e56fb7582dc821b8c7bb2cc
NutzKiller <yuvalshmuely8@gmail.com>
2025-01-28 11:31:52 +0200

    omg i hate chatgpt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,33 +1,31 @@
-# Terraform variables
+# terraform/variables.tf
 
 variable "GCP_CREDENTIALS_FILE" {
-  description = "Path to the GCP credentials JSON file"
+  description = "Path to the GCP service account credentials JSON file"
   type        = string
 }
 
 variable "GCP_PROJECT" {
-  description = "GCP project ID"
+  description = "GCP Project ID"
   type        = string
 }
 
 variable "GCP_REGION" {
-  description = "GCP region"
+  description = "GCP Region"
   type        = string
-  default     = "us-central1"
 }
 
 variable "DATABASE_URL" {
-  description = "The database connection string"
+  description = "Database connection string"
   type        = string
 }
 
 variable "SECRET_KEY" {
-  description = "The Flask application secret key"
+  description = "Flask application secret key"
   type        = string
 }
 
 variable "MY_GITHUB_TOKEN" {
-  description = "GitHub token for pushing Terraform state"
+  description = "GitHub token with permissions to push to the TF repository"
   type        = string
-  sensitive   = true
 }
