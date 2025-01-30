9088b2b21b074c6883f0c57b3922c4da8be084a3
NutzKiller <yuvalshmuely8@gmail.com>
2025-01-30 13:15:22 +0200

    bigger machine and more nodes

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -4,7 +4,7 @@
 resource "google_container_cluster" "primary" {
   name               = "gym-cluster"
   location           = var.GCP_REGION
-  initial_node_count = 1
+  initial_node_count = 3  # Increased for better scheduling
 
   remove_default_node_pool = true
 
@@ -27,14 +27,20 @@ resource "google_container_cluster" "primary" {
   }
 }
 
-# Create a node pool
+# Create a node pool with auto-scaling enabled
 resource "google_container_node_pool" "primary_nodes" {
-  cluster    = google_container_cluster.primary.name
-  location   = var.GCP_REGION
-  node_count = 1
+  cluster  = google_container_cluster.primary.name
+  location = var.GCP_REGION
+
+  # Enable auto-scaling
+  autoscaling {
+    min_node_count = 2
+    max_node_count = 3
+  }
 
   node_config {
-    machine_type = "e2-micro"
+    machine_type = "e2-medium"  # Upgraded from e2-micro to e2-medium (4GB RAM)
+    disk_size_gb = 50           # Increased disk space for better performance
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
