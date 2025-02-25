e52ee8d622b7ff6b74d3ab878295da4e435b02bd
NutzKiller <yuvalshmuely8@gmail.com>
2025-02-25 12:32:40 +0200

    work please

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "gcs" {
+    bucket = "tfstate_gymproject"
+    prefix = "terraform/state"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -123,82 +128,5 @@ resource "helm_release" "gym" {
   set {
     name  = "secretKey"
     value = var.SECRET_KEY
-  }
-}
-
-resource "null_resource" "push_to_github_after_apply" {
-  provisioner "local-exec" {
-    command = <<EOT
-      # Install Git if not already installed
-      if ! command -v git &> /dev/null; then
-        echo "Git not found. Installing..."
-        if [ -x "$(command -v apt)" ]; then
-          sudo apt update && sudo apt install -y git
-        elif [ -x "$(command -v yum)" ]; then
-          sudo yum install -y git
-        else
-          echo "Unsupported package manager. Install Git manually."
-          exit 1
-        fi
-      fi
-
-      git init
-      git config --global user.email "yuvalshmuely8@gmail.com"
-      git config --global user.name "NutzKiller"
-      git remote add origin https://${var.MY_GITHUB_TOKEN}@github.com/NutzKiller/TF.git || true
-      git fetch origin main || true
-      git checkout -B main || true
-      git pull origin main --allow-unrelated-histories --strategy-option ours || true
-      git add terraform_state/terraform.tfstate
-      git commit -m "Update Terraform state file after apply" || true
-      git push origin main --force
-    EOT
-    environment = {
-      MY_GITHUB_TOKEN = var.MY_GITHUB_TOKEN
-    }
-  }
-
-  triggers = {
-    apply_trigger = uuid()
-  }
-}
-
-resource "null_resource" "push_to_github_after_destroy" {
-  provisioner "local-exec" {
-    command = <<EOT
-      if ! command -v git &> /dev/null; then
-        echo "Git not found. Installing..."
-        if [ -x "$(command -v apt)" ]; then
-          sudo apt update && sudo apt install -y git
-        elif [ -x "$(command -v yum)" ]; then
-          sudo yum install -y git
-        else
-          echo "Unsupported package manager. Install Git manually."
-          exit 1
-        fi
-      fi
-
-      git init
-      git config --global user.email "yuvalshmuely8@gmail.com"
-      git config --global user.name "NutzKiller"
-      git remote add origin https://${var.MY_GITHUB_TOKEN}@github.com/NutzKiller/TF.git || true
-      git fetch origin main || true
-      git checkout -B main || true
-      git pull origin main --allow-unrelated-histories --strategy-option ours || true
-      git add terraform_state/terraform.tfstate
-      git commit -m "Update Terraform state file after destroy" || true
-      git push origin main --force
-    EOT
-    environment = {
-      MY_GITHUB_TOKEN = var.MY_GITHUB_TOKEN
-    }
-  }
-
-  triggers = {
-    destroy_trigger = uuid()
-  }
-
-  lifecycle {
-    create_before_destroy = false
   }
 }
